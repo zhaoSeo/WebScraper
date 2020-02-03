8aed3cc2726c31715c16c3b568ff8f940e36b451
zhao Seo <tkddnjs5652@naver.com>
2020-02-03 17:50:26 +0900

    Extracting Locations and Finishing up

--- indeed.py
+++ indeed.py
@@ -24,19 +24,29 @@ def extract_indeed_pages() :
 
     return max_page
 
+def extract_job(html) :
+    title = html.find("div", {"class" : "title"}).find("a")["title"]
+    company = html.find("span", {"class" : "company"}).string.strip()
+    
+    # company_anchor = company.find("a")
+    # if company_anchor is not None :
+    #     print(company_anchor.string)
+    # else :
+    #     print(company.string)
+
+    location = html.find("div", {"class" : "recJobLoc"})["data-rc-loc"]
+    job_id = html["data-jk"]
+
+    return {'title' : title, 'company' : company, 'location' : location, 'link' : f"https://kr.indeed.com/채용보기?jk={job_id}"}
+    
+
 def extract_indeed_jobs(last_page) :
+    jobs = []
     for page in range(last_page) :
-        jobs = []
         result = requests.get(f"{URL}&start={page*LIMIT}")
         soup = BeautifulSoup(result.text, "html.parser")
         results = soup.find_all("div", {"class" : "jobsearch-SerpJobCard"})
         for result in results :
-            title = result.find("div", {"class" : "title"}).find("a")["title"]
-            company = result.find("span", {"class" : "company"}).string.strip()
-            print(title, "-", company)
-            # company_anchor = company.find("a")
-            # if company_anchor is not None :
-            #     print(company_anchor.string)
-            # else :
-            #     print(company.string)
+            job = extract_job(result)
+            jobs.append(job)
     return jobs
