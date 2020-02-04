518080eb65c78c72bfa9af26eee62046aecdbcba
zhao Seo <tkddnjs5652@naver.com>
2020-02-04 12:45:42 +0900

    StackOverFlow extract_jobs

--- so.py
+++ so.py
@@ -9,8 +9,19 @@ def get_last_page() :
     result = requests.get(URL)
     soup = BeautifulSoup(result.text, 'html.parser')
     pagination = soup.find("div", {"class":"s-pagination"}).find_all("a")
-    print(pagination)
+    last_page = pagination[-2].get_text(strip=True)
+    return int(last_page)
+
+def extract_jobs(last_page) :
+    jobs = []
+    for page in range(last_page) :
+        result = requests.get(f"{URL}&pg={page+1}")
+        soup = BeautifulSoup(result.text, 'html.parser')
+        results = soup.find_all("div", {"class":"-job"})
+        for result in results :
+            print(result["data-jobid"])
 
 def get_jobs() :
     last_page = get_last_page()
-    return []
+    jobs = extract_jobs(last_page)
+    return jobs
